c4163ff14122a7d20db194de741bc5445336bafe
alailsonko <aalailson3@gmail.com>
2020-07-21 09:07:30 -0300

    preparing the environment frotend

---
+++ frontend/web/src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Home() {
+  return (
+      <Container>
+          <h1>hello world</h1>
+      </Container>
+  );
+}
+
+export default Home;
