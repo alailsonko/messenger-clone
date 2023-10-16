3da18d9cfe5168feab62a6b8dc2393ef64de1f4a
alailson <aalailson3@gmail.com>
2023-10-16 05:44:37 -0300

    handle error

--- packages/server/src/application/users/users.service.ts
+++ packages/server/src/application/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { SignInCommand, SignUpCommand } from './commands/impl';
 import { GetUserQuery } from './queries';
@@ -39,7 +39,7 @@ export class UsersService {
 
   getUser(findOpts: { id?: string; email?: string; username?: string }) {
     if (R.isEmpty(findOpts)) {
-      throw new Error('missing parameter: id or email or username');
+      throw new BadRequestException();
     }
     return this.queryBus.execute<GetUserQuery, UsersEntity>(
       new GetUserQuery(findOpts),
