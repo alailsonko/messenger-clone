e30640963e6f29ff44d2279c5f40357c31325cd2
alailson <aalailson3@gmail.com>
2024-01-21 16:08:56 -0300

    Add new modules and routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,17 +1,20 @@
 import { RouteObject, useRoutes } from 'react-router-dom'
 import { Landing } from '../pages/Landing'
 import { publicRoutes } from './public'
+import { protectedRoutes } from './protected'
 
 
 export const AppRoutes = () => {
     const commonRoutes: RouteObject[] = [
         { 
             path: '/landing',
-            element: <Landing />
+            element: <Landing />,
      }
     ]
 
-    const element = useRoutes([...commonRoutes, ...publicRoutes])
+    const routes = true ? protectedRoutes : publicRoutes
+
+    const element = useRoutes([...commonRoutes, ...routes])
 
     return  <>{element}</>
 }
---
+++ web/src/routes/protected.tsx
@@ -0,0 +1,9 @@
+import { RouteObject } from "react-router-dom";
+import { Login } from "../pages/Login";
+
+export const protectedRoutes: RouteObject[] = [
+    {
+        path: '/',
+        element: <Login />
+    },
+]
