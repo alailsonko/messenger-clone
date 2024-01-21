c3cc10fc7075c99da8c04ba1201e26e4c6a750dc
alailson <aalailson3@gmail.com>
2024-01-21 17:46:59 -0300

    Update UsersEntity validation in users.entity.spec.ts

--- server/src/domain/users/__tests__/users.entity.spec.ts
+++ server/src/domain/users/__tests__/users.entity.spec.ts
@@ -1,24 +1,55 @@
-import { UserEntity } from '../users.entity';
+import { USER_OPERATIONS, UsersEntity } from '../users.entity';
 
 describe('UserEntity', () => {
-  it('should create a valid user entity', async () => {
-    const validUser = new UserEntity(
-      '550e8400-e29b-41d4-a716-446655440000',
-      'john_doe',
-      'password123',
-      'john@example.com',
-      new Date(),
-      new Date(),
-      null,
+  it('should update a valid user entity', async () => {
+    const validationErrorsOne = await UsersEntity.validate(
+      {
+        username: 'john_doe',
+        password: 'password123',
+        email: 'john@example.com',
+      },
+      USER_OPERATIONS.UPDATE,
+    );
+    const validationErrorsTwo = await UsersEntity.validate(
+      {
+        username: 'john_doe',
+      },
+      USER_OPERATIONS.UPDATE,
+    );
+    const validationErrorsThree = await UsersEntity.validate(
+      {
+        password: 'password123',
+      },
+      USER_OPERATIONS.UPDATE,
+    );
+    const validationErrorsFourth = await UsersEntity.validate(
+      {
+        email: 'john@example.com',
+      },
+      USER_OPERATIONS.UPDATE,
     );
 
-    const validationErrors = await validUser.validate();
+    expect(validationErrorsOne).toHaveLength(0);
+    expect(validationErrorsTwo).toHaveLength(0);
+    expect(validationErrorsThree).toHaveLength(0);
+    expect(validationErrorsFourth).toHaveLength(0);
+  });
+
+  it('should create a valid user entity', async () => {
+    const validationErrors = await UsersEntity.validate(
+      {
+        username: 'john_doe',
+        password: 'password123',
+        email: 'john@example.com',
+      },
+      USER_OPERATIONS.CREATE,
+    );
 
     expect(validationErrors).toHaveLength(0);
   });
 
   it('should detect validation errors for an invalid user entity', async () => {
-    const invalidUser = new UserEntity(
+    const invalidUser = new UsersEntity(
       '550e8400-e29b-41d4-a716-446655440000',
       '', // Empty username (invalid)
       'pass', // Password length less than 6 (invalid)
@@ -28,13 +59,16 @@ describe('UserEntity', () => {
       null,
     );
 
-    const validationErrors = await invalidUser.validate();
+    const validationErrors = await UsersEntity.validate(
+      invalidUser,
+      USER_OPERATIONS.READ,
+    );
 
     expect(validationErrors).not.toHaveLength(0);
   });
 
   it('should convert the entity to a plain object', () => {
-    const user = new UserEntity(
+    const user = new UsersEntity(
       '550e8400-e29b-41d4-a716-446655440000',
       'john_doe',
       'password123',
