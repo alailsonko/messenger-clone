08646979c2fa10066c467d3486dfb9245a928c37
alailsonko <aalailson3@gmail.com>
2025-04-26 22:27:59 +0200

    update go.sum with new dependencies; clean up main.go by removing unused code

--- modules/shared/main.go
+++ modules/shared/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"log"
-
-	users_pb "github.com/alailsonko/messenger-clone/modules/shared/protobuf"
+	_ "github.com/alailsonko/messenger-clone/modules/shared/logger"
+	_ "github.com/alailsonko/messenger-clone/modules/shared/protobuf"
 )
 
 func main() {
-	log.Println("Hello, World!")
-	req := users_pb.ListUsersRequest{
-		Page:     1,
-		PageSize: 10,
-	}
-	log.Printf("Request created: %+v", req)
 }
